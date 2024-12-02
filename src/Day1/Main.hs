213adf415c8a9e23b53c8a7051f272bb5b2f4f26
Jimmy <jimmyhogoboom@gmail.com>
2024-12-02 13:20:43 -0600

    Day 1, Part 1 solution

--- src/Day1/Main.hs
+++ src/Day1/Main.hs
@@ -1,13 +1,31 @@
 module Day1.Main (part1) where
 
--- TODO: 
--- 1. Read file
--- 2. Split input
--- 3. Run
+import Data.List
+import Data.List.Split(split)
 
 day1InputFile = "data/Day1/input.txt"
+day1ExampleFile = "data/Day1/example.txt"
 
-part1 :: IO String
+part1 :: IO Integer
 part1 = do
   contents <- readFile day1InputFile
-  return contents
+  -- contents <- readFile day1ExampleFile
+  let result = calculateTotal $ parseLists contents
+  -- print $ result
+  return result
+
+parseLists :: String -> ([Integer], [Integer])
+parseLists xs = unzip $ map pairSplit (lines xs)
+  where pairSplit x = tuplify (words x)
+        tuplify [x,y] = (read x, read y)
+
+distance :: Integer -> Integer -> Integer
+distance x y = abs $ x - y
+
+distances :: [Integer] -> [Integer] -> [Integer]
+distances xs ys = zipWith distance xs' ys'
+  where xs' = sort xs
+        ys' = sort ys
+
+calculateTotal :: ([Integer], [Integer]) -> Integer
+calculateTotal = sum . uncurry distances
