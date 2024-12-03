821839173c61c276f7145393d4c20b82670cc0d3
Jimmy <jimmyhogoboom@gmail.com>
2024-12-02 23:51:02 -0600

    Day 1 Part 2 solution

--- app/Main.hs
+++ app/Main.hs
@@ -6,4 +6,6 @@ main :: IO ()
 main = do
   p1 <- part1
   print p1
+  p2 <- part2
+  print p2
   return ()
--- src/Day1/Main.hs
+++ src/Day1/Main.hs
@@ -1,4 +1,4 @@
-module Day1.Main (part1) where
+module Day1.Main (part1, part2) where
 
 import Data.List
 
@@ -8,7 +8,7 @@ day1InputFile = "data/Day1/input.txt"
 day1ExampleFile :: String
 day1ExampleFile = "data/Day1/example.txt"
 
-part1 :: IO Integer
+part1 :: IO Int
 part1 = do
   contents <- readFile day1InputFile
   -- contents <- readFile day1ExampleFile
@@ -16,21 +16,41 @@ part1 = do
   -- print $ result
   return result
 
-parseLists :: String -> ([Integer], [Integer])
+parseLists :: String -> ([Int], [Int])
 parseLists xs = unzip $ map pairSplit (lines xs)
   where
     pairSplit x = tuplify (words x)
     tuplify [x, y] = (read x, read y)
     tuplify _ = (0, 0)
 
-distance :: Integer -> Integer -> Integer
+distance :: Int -> Int -> Int
 distance x y = abs $ x - y
 
-distances :: [Integer] -> [Integer] -> [Integer]
+distances :: [Int] -> [Int] -> [Int]
 distances xs ys = zipWith distance xs' ys'
   where
     xs' = sort xs
     ys' = sort ys
 
-calculateTotal :: ([Integer], [Integer]) -> Integer
+calculateTotal :: ([Int], [Int]) -> Int
 calculateTotal = sum . uncurry distances
+
+----
+
+part2 :: IO Int
+part2 = do
+  contents <- readFile day1InputFile
+  let result = similarity $ parseLists contents
+  print result
+  return result
+
+countWith :: (a -> Bool) -> [a] -> Int
+countWith p = length . filter p
+
+countOf :: (Eq a) => a -> [a] -> Int
+countOf a = countWith (a ==)
+
+similarity :: ([Int], [Int]) -> Int
+similarity (xs, ys) = foldr simScore 0 xs
+  where
+    simScore a b = b + (a * countOf a ys)
