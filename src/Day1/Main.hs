1d442d890c8dbb0b592ab4f7684b11ce646814b2
Jimmy <jimmyhogoboom@gmail.com>
2024-12-02 23:06:04 -0600

    apply linting

--- src/Day1/Main.hs
+++ src/Day1/Main.hs
@@ -1,9 +1,11 @@
 module Day1.Main (part1) where
 
 import Data.List
-import Data.List.Split(split)
 
+day1InputFile :: String
 day1InputFile = "data/Day1/input.txt"
+
+day1ExampleFile :: String
 day1ExampleFile = "data/Day1/example.txt"
 
 part1 :: IO Integer
@@ -16,16 +18,19 @@ part1 = do
 
 parseLists :: String -> ([Integer], [Integer])
 parseLists xs = unzip $ map pairSplit (lines xs)
-  where pairSplit x = tuplify (words x)
-        tuplify [x,y] = (read x, read y)
+  where
+    pairSplit x = tuplify (words x)
+    tuplify [x, y] = (read x, read y)
+    tuplify _ = (0, 0)
 
 distance :: Integer -> Integer -> Integer
 distance x y = abs $ x - y
 
 distances :: [Integer] -> [Integer] -> [Integer]
 distances xs ys = zipWith distance xs' ys'
-  where xs' = sort xs
-        ys' = sort ys
+  where
+    xs' = sort xs
+    ys' = sort ys
 
 calculateTotal :: ([Integer], [Integer]) -> Integer
 calculateTotal = sum . uncurry distances
