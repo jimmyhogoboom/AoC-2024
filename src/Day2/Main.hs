e9440c0761815cfeff803279c5f6e763a33f9331
Jimmy <jimmyhogoboom@gmail.com>
2025-01-07 23:47:15 -0600

    Day 2 Part 2 WIP

--- src/Day2/Main.hs
+++ src/Day2/Main.hs
@@ -1,6 +1,6 @@
-module Day2.Main (part1) where
+module Day2.Main where
 
-import Util (countWith)
+import Util (countWith, deleteAt)
 
 day2InputFile :: String
 day2InputFile = "data/Day2/input.txt"
@@ -50,3 +50,99 @@ inBounds bounds = go
 
 isSafe :: Report -> Bool
 isSafe r = (increasing r || decreasing r) && inBounds (1, 3) r
+
+---
+
+-- TODO: Should be true: [43,41,43,44,45,47,49]
+-- Removing the first item makes it safe
+
+part2 :: IO Int
+part2 = do
+  contents <- readFile day2InputFile
+  -- contents <- readFile day2ExampleFile
+  let parsed = parseList contents
+  -- let result = map (isSafeWithTolerance 1) parsed
+  -- print $ zip result parsed
+  -- return $ countWith id result
+  return 0
+
+-- TODO: If a fault is found, try every permutation of the Report with a value missing, both tests
+
+type FaultTolerance = Int
+
+inBounds' :: Bounds -> Int -> Int -> Bool
+inBounds' (bot, top) x y = dif >= bot && dif <= top
+  where
+    dif = abs $ x - y
+
+safelyIncreasing :: Bounds -> Int -> Int -> Bool
+safelyIncreasing b x y = x < y && inBounds' b x y
+
+safelyDecreasing :: Bounds -> Int -> Int -> Bool
+safelyDecreasing b x y = x > y && inBounds' b x y
+
+-- increasingWithTolerance :: Bounds -> FaultTolerance -> Report -> Bool
+
+-- if not increasing, try with each value missing until it is
+-- might have to do a second layer of recursion to test all those
+
+-- type FaultCount = Int
+
+-- data FaultTolerantResult = FaultTolerantResult
+--   { safe :: Bool,
+--     verifiedReport :: Report,
+--     faultCount :: Int
+--   }
+--   deriving (Show)
+
+-- increasingWithTolerance :: FaultTolerance -> Report -> FaultTolerantResult
+-- increasingWithTolerance t r = go r r 0 0
+--   where
+--     go :: Report -> Report -> Int -> FaultCount -> FaultTolerantResult
+--     go r' (a : b : ls) i faults
+--       | a > b && faults >= t = FaultTolerantResult {safe = False, verifiedReport = r', faultCount = faults + 1} -- If this is another fault and faults are already at the limit
+--       | a > b =
+--           let newReportB = deleteAt (i + 1) r
+--               newReportA = deleteAt i r
+--               resultA = go newReportA newReportA 0 (faults + 1)
+--               resultB = go newReportB newReportB 0 (faults + 1)
+--            in if safe resultA then resultA else if safe resultB then resultB else FaultTolerantResult {safe = False} -- Try again without the faulty value
+--       | otherwise = go r' (b : ls) (i + 1) faults -- continue to next index
+--     go r' _ _ faults = FaultTolerantResult {safe = faults <= t, verifiedReport = r', faultCount = faults}
+
+-- decreasingWithTolerance :: FaultTolerance -> Report -> FaultTolerantResult
+-- decreasingWithTolerance t r = go r r 0 0
+--   where
+--     go :: Report -> Report -> Int -> FaultCount -> FaultTolerantResult
+--     go r' (a : b : ls) i faults
+--       | a < b && faults >= t = FaultTolerantResult {safe = False, verifiedReport = r', faultCount = faults + 1} -- If this is another fault and faults are already at the limit
+--       | a < b =
+--           let newReportB = deleteAt (i + 1) r
+--               newReportA = deleteAt i r
+--               resultA = go newReportA newReportA 0 (faults + 1)
+--               resultB = go newReportB newReportB 0 (faults + 1)
+--            in if safe resultA then resultA else if safe resultB then resultB else FaultTolerantResult {safe = False} -- Try again without the faulty value
+--       | otherwise = go r' (b : ls) (i + 1) faults -- continue to next index
+--     go r' _ _ faults = FaultTolerantResult {safe = faults <= t, verifiedReport = r', faultCount = faults}
+
+-- inBoundsWithTolerance :: FaultTolerance -> Bounds -> Report -> FaultCount -> Bool
+-- inBoundsWithTolerance t bounds r = go r 0
+--   where
+--     bot = fst bounds
+--     top = snd bounds
+--     go (a : b : rs) i faults =
+--       let dif = abs $ a - b
+--        in if dif < bot || dif > top
+--             then faults < t && go (deleteAt (i + 1) r) 0 (faults + 1)
+--             else go (b : rs) (i + 1) faults
+--     go _ _ faults = faults <= t
+
+-- isSafeWithTolerance :: FaultTolerance -> Report -> Bool
+-- isSafeWithTolerance t r =
+--   let increasingResult = increasingWithTolerance t r
+--       decreasingResult = decreasingWithTolerance t r
+--       result
+--         | safe increasingResult = increasingResult
+--         | safe decreasingResult = decreasingResult
+--         | otherwise = FaultTolerantResult {safe = False, verifiedReport = []}
+--    in safe result && inBoundsWithTolerance t (1, 3) (verifiedReport result) (faultCount result)
--- src/Util.hs
+++ src/Util.hs
@@ -1,7 +1,10 @@
-module Util (countWith, countOf) where
+module Util (countWith, countOf, deleteAt) where
 
 countWith :: (a -> Bool) -> [a] -> Int
 countWith p = length . filter p
 
 countOf :: (Eq a) => a -> [a] -> Int
 countOf a = countWith (a ==)
+
+deleteAt :: Int -> [a] -> [a]
+deleteAt i xs = take i xs ++ drop (i + 1) xs
